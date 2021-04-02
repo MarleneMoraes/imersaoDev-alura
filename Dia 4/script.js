3e43f5113102867c45087698e8c78d79e620bdd4
Marlene Vasconcelos Moraes de Oliveira <marlenevmoraes@outlook.com>
2021-04-02 01:18:49 -0300

    atualizacao dia 4

--- Dia 4/script.js
+++ Dia 4/script.js
@@ -1,5 +1,11 @@
-var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BYTM5MDg3MGMtODAyYy00MGQ0LWJhM2MtZDFmMjAzNTFhZGRmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOWFhY2JkYmItM2U0MC00NjM5LTk4N2UtYTY2MTZmNmEzMTQyXkEyXkFqcGdeQXVyMTY2MzYyNzA@._V1_UY268_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BYzBhOWU4ODAtZDYzYi00NDU1LWIzZWUtNDZmMDgxODljZTVmXkEyXkFqcGdeQXVyMTAwMzM3NDI3._V1_UX182_CR0,0,182,268_AL_.jpg"]
+var listaPoster = ["https://m.media-amazon.com/images/M/MV5BYTM5MDg3MGMtODAyYy00MGQ0LWJhM2MtZDFmMjAzNTFhZGRmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOWFhY2JkYmItM2U0MC00NjM5LTk4N2UtYTY2MTZmNmEzMTQyXkEyXkFqcGdeQXVyMTY2MzYyNzA@._V1_UY268_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BYzBhOWU4ODAtZDYzYi00NDU1LWIzZWUtNDZmMDgxODljZTVmXkEyXkFqcGdeQXVyMTAwMzM3NDI3._V1_UX182_CR0,0,182,268_AL_.jpg"]
 
-for (var i=0; i < listaFilmes.length; i++){
-    document.write("<img src=" + listaFilmes[i] + ">")
+var listaTitulo = ["Cidade Invisível", "M-8: Quando a Morte Socorre a Vida", "Project Power"]
+
+for (var i=0; i < listaPoster.length; i++){
+    document.write("<img src=" + listaPoster[i] + ">")
+}
+
+for (var i=0; i < listaTitulo.length; i++){
+    document.write("<p" + listaTitulo[i] + ">")
 }
